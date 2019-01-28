02acc754ce7d18b63d1acd8e0655e1d4bed726d5
KaiserKatze <donizyo@gmail.com>
2019-01-28 23:49:41 +0800

    Test

--- modmain.lua
+++ modmain.lua
@@ -13,3 +13,21 @@ PrefabFiles =
   "alloy",
   "iron",
 }
+
+if DEBUG then
+  local SpawnPrefab = _G.SpawnPrefab
+  AddPlayerPostInit(function(inst)
+    local function giveitem(item_name)
+      local item = SpawnPrefab(item_name)
+      if item then
+        inst.components.inventory:GiveItem(item)
+      end
+    end
+
+    -- Spawn items in tester's inventory
+    if inst.components.inventory then
+      giveitem("iron")
+      giveitem("alloy")
+    end
+  end)
+end
