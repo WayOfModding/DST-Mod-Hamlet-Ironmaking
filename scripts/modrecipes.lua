b8fa47dc10af5bf7979df3d0297df0d4e3711c4e
KaiserKatze <donizyo@gmail.com>
2019-01-29 12:55:46 +0800

    Format

--- scripts/modrecipes.lua
+++ scripts/modrecipes.lua
@@ -1,3 +1,29 @@
-Recipe("smelter", {Ingredient("cutstone", 6), Ingredient("boards", 4), Ingredient("redgem", 1)}, RECIPETABS.SCIENCE, TECH.SCIENCE_TWO, RECIPE_GAME_TYPE.PORKLAND, "smetler_placer")
-Recipe("antmaskhat", {Ingredient("chitin", 5),Ingredient("footballhat", 1)}, RECIPETABS.WAR, TECH.SCIENCE_ONE, RECIPE_GAME_TYPE.PORKLAND)
-Recipe("antsuit", {Ingredient("chitin", 5),Ingredient("armorwood", 1)}, RECIPETABS.WAR, TECH.SCIENCE_ONE, RECIPE_GAME_TYPE.PORKLAND)
+Recipe(
+  "smelter",
+  {
+    Ingredient("cutstone", 6),
+    Ingredient("boards", 4),
+    Ingredient("redgem", 1)
+  },
+  RECIPETABS.SCIENCE,
+  TECH.SCIENCE_TWO,
+  "smetler_placer"
+)
+Recipe(
+  "antmaskhat",
+  {
+    Ingredient("chitin", 5),
+    Ingredient("footballhat", 1)
+  },
+  RECIPETABS.WAR,
+  TECH.SCIENCE_ONE,
+)
+Recipe(
+  "antsuit",
+  {
+    Ingredient("chitin", 5),
+    Ingredient("armorwood", 1)
+  },
+  RECIPETABS.WAR,
+  TECH.SCIENCE_ONE,
+)
