cd40163fe0abbc8eddd770de9c75fccef66b4d50
KaiserKatze <donizyo@gmail.com>
2019-01-29 12:52:35 +0800

    Copy from Hamlet

---
+++ scripts/modrecipes.lua
@@ -0,0 +1,3 @@
+Recipe("smelter", {Ingredient("cutstone", 6), Ingredient("boards", 4), Ingredient("redgem", 1)}, RECIPETABS.SCIENCE, TECH.SCIENCE_TWO, RECIPE_GAME_TYPE.PORKLAND, "smetler_placer")
+Recipe("antmaskhat", {Ingredient("chitin", 5),Ingredient("footballhat", 1)}, RECIPETABS.WAR, TECH.SCIENCE_ONE, RECIPE_GAME_TYPE.PORKLAND)
+Recipe("antsuit", {Ingredient("chitin", 5),Ingredient("armorwood", 1)}, RECIPETABS.WAR, TECH.SCIENCE_ONE, RECIPE_GAME_TYPE.PORKLAND)
